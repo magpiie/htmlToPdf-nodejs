4be802976965365a493753313505e71e95e751ea
JeongJun Jo <rgfdds98.dev@gmail.com>
2023-12-29 15:40:06 +0900

    Style: create .prettierrc.json

--- converToPDF.js
+++ converToPDF.js
@@ -1,19 +1,19 @@
 // convertToPDF.js
 
-const puppeteer = require("puppeteer");
-const fs = require("fs");
+const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 async function convertToPDF(htmlContent, outputPath) {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
 
-  // HTML 문자열을 로드
-  await page.setContent(htmlContent, { waitUntil: "networkidle2" });
+	// HTML 문자열을 로드
+	await page.setContent(htmlContent, { waitUntil: 'networkidle2' });
 
-  // PDF로 변환
-  await page.pdf({ path: outputPath, format: "A4" });
+	// PDF로 변환
+	await page.pdf({ path: outputPath, format: 'A4' });
 
-  await browser.close();
+	await browser.close();
 }
 
 module.exports = convertToPDF;
