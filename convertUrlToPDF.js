53d96a47fc31cc59bdce34eb874d160e2d9fa8e9
JeongJun Jo <rgfdds98.dev@gmail.com>
2024-01-02 14:56:58 +0900

    Fix: change logic for maintaining CSS style in page contents
    resolve problem with CSS not being maintained

--- convertUrlToPDF.js
+++ convertUrlToPDF.js
@@ -1,17 +1,11 @@
 // convertUrlToPDF.js
 
 const puppeteer = require('puppeteer');
-const axios = require('axios');
-const fs = require('fs');
 
 async function convertUrlToPDF() {
 	// const url = 'https://github.com/';
 	const url = 'http://127.0.0.1:5500/report-frame/josa4stepReport_1.html';
 	const outputPath = 'D:/TEST/htmlToPdf-test/output/report.pdf';
-
-	// URL로부터 HTML 가져오기
-	const response = await axios.get(url);
-	const htmlContent = response.data;
 
 	// HTML을 PDF로 변환 시작 시간 기록
 	const startTime = new Date();
@@ -20,7 +14,7 @@ async function convertUrlToPDF() {
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
 
-	await page.setContent(htmlContent, { waitUntil: 'networkidle2' });
+	await page.goto(url, { waitUntil: 'domcontentloaded' });
 
 	// A4 크기 설정 (가로 방향)
 	await page.pdf({
