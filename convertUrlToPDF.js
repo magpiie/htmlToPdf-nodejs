8b443ef18e64bccca4d4acc31b7760b15994d7da
JeongJun Jo <rgfdds98.dev@gmail.com>
2024-01-02 15:15:19 +0900

    Feat: add loop logic in convert fn

--- convertUrlToPDF.js
+++ convertUrlToPDF.js
@@ -3,25 +3,27 @@
 const puppeteer = require('puppeteer');
 
 async function convertUrlToPDF() {
-	// const url = 'https://github.com/';
-	const url = 'http://127.0.0.1:5500/report-frame/josa4stepReport_1.html';
-	const outputPath = 'D:/TEST/htmlToPdf-test/output/report.pdf';
-
 	// HTML을 PDF로 변환 시작 시간 기록
 	const startTime = new Date();
 
-	// HTML을 PDF로 변환
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
 
-	await page.goto(url, { waitUntil: 'domcontentloaded' });
+	for (let i = 1; i < 10; i++) {
+		const url = `http://127.0.0.1:5500/report-frame/josa4stepReport_${i}.html`;
+		const outputPath = `D:/TEST/htmlToPdf-test/output/report_${i}.pdf`;
 
-	// A4 크기 설정 (가로 방향)
-	await page.pdf({
-		path: outputPath,
-		format: 'A4',
-		landscape: false, // 가로 방향으로 설정
-	});
+		// HTML을 PDF로 변환
+
+		await page.goto(url, { waitUntil: 'domcontentloaded' });
+
+		// A4 크기 설정 (가로 방향)
+		await page.pdf({
+			path: outputPath,
+			format: 'A4',
+			landscape: false, // 가로 방향으로 설정
+		});
+	}
 
 	// HTML을 PDF로 변환 종료 시간 기록
 	const endTime = new Date();
